14ce3ac5bf9a937a409086f1d07c41912674797f
David Gilbertson <gilbertson.david@gmail.com>
2016-12-11 15:31:02 +1100

    Add help link

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -19,6 +19,13 @@ class App extends React.Component {
       <div>
         <header className="header">
           <h1 className="header__title">Know It All</h1>
+
+          <a
+            href="https://hackernoon.com/what-you-dont-know-about-web-development-d7d631f5d468#.ex2yp6d64"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="header__help"
+          >What is this?</a>
         </header>
 
         <SkillTable {...this.state} />
