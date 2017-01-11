536ebf2613557147f7689112f51f64ef0066ecc1
David Gilbertson <gilbertson.david@gmail.com>
2017-01-11 20:33:24 +1100

    Ease up on the warnings

--- app/utils/swapNodes.js
+++ app/utils/swapNodes.js
@@ -2,11 +2,11 @@ export default (oldNode, newNode, suppressWarnings) => {
   if (oldNode.parentElement) {
     // TODO (davidg): check this isn't a memory leak waiting to happen
     oldNode.parentElement.replaceChild(newNode, oldNode);
-  } else {
+  } else if (window.APP_DEBUG) {
     console.error(`Can't replace node because it doesn't have a parent`, oldNode);
   }
 
-  if (!suppressWarnings && oldNode.isEqualNode(newNode)) {
+  if (window.APP_DEBUG && !suppressWarnings && oldNode.isEqualNode(newNode)) {
     console.warn(`A node was updated but there was no change in the rendered output`, newNode);
   }
 
